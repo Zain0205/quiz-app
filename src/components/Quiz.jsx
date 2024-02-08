515169e99cc9d022ce21d74971a31377a4f9d773
Fahmi Zain <fahmizain000@gmail.com>
2024-02-08 12:28:05 +0700

    feat: creating Quiz component

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import QUESTION from "../question.js";
+
+function Quiz() {
+  const [userAsnwer, setUserAnswer] = useState([]);
+  const questionIndex = userAsnwer.length;
+
+  const handleUserAnswer = (ans) => {
+    setUserAnswer((prev) => {
+      return [...prev, ans];
+    });
+  };
+
+  if (userAsnwer.length === QUESTION.length) {
+    return (
+      <div className="bg-sky-900 py-10 flex flex-col gap-y-3 items-center justify-center">
+        <p className="text-2xl text-white font-semibold">Quiz Selesai</p>
+        <button className="bg-sky-600 px-8 py-2 rounded-xl text-slate-50 font-semibold hover:bg-indigo-600">
+          Mulai Lagi?
+        </button>
+      </div>
+    );
+  }
+
+  const questionAsnwer = [...QUESTION[questionIndex].answers];
+  const unSortedAnswer = questionAsnwer.sort(() => Math.random() - 0.5);
+
+  return (
+    <div className="bg-sky-900 text-white w-full rounded-md px-4 flex flex-col gap-y-5 items-center py-5">
+      <h1 className="text-2xl font-semibold">{QUESTION[questionIndex].question}</h1>
+      <ul className="text-center flex flex-col gap-y-5 w-full">
+        {unSortedAnswer.map((answer, i) => (
+          <li className="w-full bg-sky-700 rounded-full text-xl hover:bg-indigo-600 cursor-pointer" key={i}>
+            <button onClick={() => handleUserAnswer(answer)} className="w-full py-2">
+              {answer}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Quiz;
