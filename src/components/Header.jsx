9a185109874e5faa62716ccbd8b7c31e67ed1512
Fahmi Zain <fahmizain000@gmail.com>
2024-02-08 12:27:22 +0700

    feat: adding header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <header className="text-center bg-sky-900 p-4 rounded-md">
+        <h1 className="text-2xl font-semibold text-white">Ini Quiz Tod</h1>
+      </header>
+    </>
+  );
+}
+
+export default Header;
