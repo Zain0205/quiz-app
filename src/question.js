9c62b3bbc67654e90b310172abdc4ae2fd761973
Fahmi Zain <fahmizain000@gmail.com>
2024-02-08 12:27:34 +0700

    Create question.js

---
+++ src/question.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    id: "q1",
+    question: "Pertanyaan 1",
+    answers: ["benar", "salah", "salah", "salah"],
+  },
+  {
+    id: "q2",
+    question: "Pertanyaan 2",
+    answers: ["benar", "salah", "salah", "salah"],
+  },
+  {
+    id: "q3",
+    question: "Pertanyaan 3",
+    answers: ["benar", "salah", "salah", "salah"],
+  },
+  {
+    id: "q4",
+    question: "Pertanyaan 4",
+    answers: ["benar", "salah", "salah", "salah"],
+  },
+  {
+    id: "q5",
+    question: "Pertanyaan 5",
+    answers: ["benar", "salah", "salah", "salah"],
+  },
+  {
+    id: "q6",
+    question: "Pertanyaan 6",
+    answers: ["benar", "salah", "salah", "salah"],
+  },
+  {
+    id: "q7",
+    question: "Pertanyaan 7",
+    answers: ["benar", "salah", "salah", "salah"],
+  },
+  {
+    id: "q8",
+    question: "Pertanyaan 8",
+    answers: ["benar", "salah", "salah", "salah"],
+  },
+  {
+    id: "q9",
+    question: "Pertanyaan 9",
+    answers: ["benar", "salah", "salah", "salah"],
+  },
+  {
+    id: "q10",
+    question: "Pertanyaan 10",
+    answers: ["benar", "salah", "salah", "salah"],
+  },
+];
