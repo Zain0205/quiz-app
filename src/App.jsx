377057cb61a2c5be19fb9b6b569c1e15e54d247e
Fahmi Zain <fahmizain000@gmail.com>
2024-02-08 12:28:24 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-
+import Header from "./components/Header";
+import Quiz from "./components/Quiz";
 
 function App() {
-
   return (
     <>
-      <h1 className='text-center text-4xl'>Coming Soon...</h1>
+      <div className="bg-sky-500 h-screen px-4 flex flex-col justify-center gap-y-6">
+        <Header />
+        <Quiz />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
