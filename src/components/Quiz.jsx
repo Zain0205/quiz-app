b50c37364647b995f271766a4baa05b237e97e78
Fahmi Zain <fahmizain000@gmail.com>
2024-02-10 09:31:09 +0700

    Update Quiz components, highlighting the answer

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,16 +1,34 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useRef } from "react";
 import QUESTION from "../question.js";
 import Timer from "./TImer.jsx";
 
 function Quiz() {
+  const shuffeledAnswer = useRef();
+  const [asnwerState, setAnswerState] = useState("");
   const [userAsnwer, setUserAnswer] = useState([]);
-  const questionIndex = userAsnwer.length;
+  const questionIndex = asnwerState === "" ? userAsnwer.length : userAsnwer.length - 1;
 
-  const handleUserAnswer = useCallback((ans) => {
-    setUserAnswer((prev) => {
-      return [...prev, ans];
-    });
-  }, []);
+  const handleUserAnswer = useCallback(
+    (ans) => {
+      setAnswerState("answered");
+      setUserAnswer((prev) => {
+        return [...prev, ans];
+      });
+
+      setTimeout(() => {
+        if (QUESTION[questionIndex].answers[0] === ans) {
+          setAnswerState("correct");
+        } else {
+          setAnswerState("wrong");
+        }
+
+        setTimeout(() => {
+          setAnswerState("");
+        }, 2000);
+      }, 1000);
+    },
+    [questionIndex]
+  );
 
   const handleSkipAnswer = useCallback(() => handleUserAnswer(null), [handleUserAnswer]);
 
@@ -18,28 +36,44 @@ function Quiz() {
     return (
       <div className="bg-sky-900 py-10 flex flex-col gap-y-3 items-center justify-center">
         <p className="text-2xl text-white font-semibold">Quiz Selesai</p>
-        <button className="bg-sky-600 px-8 py-2 rounded-xl text-slate-50 font-semibold hover:bg-indigo-600">
-          Mulai Lagi?
-        </button>
+        <button className="bg-sky-600 px-8 py-2 rounded-xl text-slate-50 font-semibold hover:bg-indigo-600">Mulai Lagi?</button>
       </div>
     );
   }
 
-  const questionAsnwer = [...QUESTION[questionIndex].answers];
-  const unSortedAnswer = questionAsnwer.sort(() => Math.random() - 0.5);
+  if (!shuffeledAnswer.current) {
+    shuffeledAnswer.current = [...QUESTION[questionIndex].answers];
+    shuffeledAnswer.current.sort(() => Math.random() - 0.5);
+  }
 
   return (
     <div className="bg-sky-900 text-white w-full rounded-md px-4 flex flex-col gap-y-5 items-center py-5">
       <Timer key={questionIndex} timeout={10000} onTimeOut={() => handleSkipAnswer(null)} />
       <h1 className="text-2xl font-semibold">{QUESTION[questionIndex].question}</h1>
       <ul className="text-center flex flex-col gap-y-5 w-full">
-        {unSortedAnswer.map((answer, i) => (
-          <li className="w-full bg-sky-700 rounded-full text-xl hover:bg-indigo-600 cursor-pointer" key={i}>
-            <button onClick={() => handleUserAnswer(answer)} className="w-full py-2">
-              {answer}
-            </button>
-          </li>
-        ))}
+        {shuffeledAnswer.current.map((answer, i) => {
+          let cssStyle = "";
+          const isSelected = userAsnwer[userAsnwer.length - 1] === answer;
+
+          if (asnwerState === "answered" && isSelected) {
+            cssStyle = "bg-amber-500";
+          } 
+          
+          if (asnwerState === "wrong" && isSelected) {
+            cssStyle = "bg-red-500";
+          } else if (asnwerState === "correct" && isSelected) {
+            cssStyle = "bg-lime-500";
+          } else if (asnwerState === "") {
+            cssStyle = "bg-sky-700";
+          }
+          return (
+            <li className={`w-full bg-sky-700 rounded-full text-xl hover:bg-indigo-600 cursor-pointer`} key={i}>
+              <button onClick={() => handleUserAnswer(answer)} className={`w-full py-2 rounded-full ${cssStyle}`}>
+                {answer}
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
