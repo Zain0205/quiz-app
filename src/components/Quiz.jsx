c89ac3faba4739a303d8d39f64a31a6aee1d2e5b
Fahmi Zain <fahmizain000@gmail.com>
2024-02-12 04:49:19 +0100

    Update Quiz,jsx

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,10 @@
 import { useState, useCallback, useRef } from "react";
 import QUESTION from "../question.js";
 import Timer from "./TImer.jsx";
+import Answer from "./Answer.jsx";
 
 function Quiz() {
-  const shuffeledAnswer = useRef();
-  const [asnwerState, setAnswerState] = useState("");
+    const [asnwerState, setAnswerState] = useState("");
   const [userAsnwer, setUserAnswer] = useState([]);
   const questionIndex = asnwerState === "" ? userAsnwer.length : userAsnwer.length - 1;
 
@@ -41,40 +41,13 @@ function Quiz() {
     );
   }
 
-  if (!shuffeledAnswer.current) {
-    shuffeledAnswer.current = [...QUESTION[questionIndex].answers];
-    shuffeledAnswer.current.sort(() => Math.random() - 0.5);
-  }
+  
 
   return (
     <div className="bg-sky-900 text-white w-full rounded-md px-4 flex flex-col gap-y-5 items-center py-5">
       <Timer key={questionIndex} timeout={10000} onTimeOut={() => handleSkipAnswer(null)} />
       <h1 className="text-2xl font-semibold">{QUESTION[questionIndex].question}</h1>
-      <ul className="text-center flex flex-col gap-y-5 w-full">
-        {shuffeledAnswer.current.map((answer, i) => {
-          let cssStyle = "";
-          const isSelected = userAsnwer[userAsnwer.length - 1] === answer;
-
-          if (asnwerState === "answered" && isSelected) {
-            cssStyle = "bg-amber-500";
-          } 
-          
-          if (asnwerState === "wrong" && isSelected) {
-            cssStyle = "bg-red-500";
-          } else if (asnwerState === "correct" && isSelected) {
-            cssStyle = "bg-lime-500";
-          } else if (asnwerState === "") {
-            cssStyle = "bg-sky-700";
-          }
-          return (
-            <li className={`w-full bg-sky-700 rounded-full text-xl hover:bg-indigo-600 cursor-pointer`} key={i}>
-              <button onClick={() => handleUserAnswer(answer)} className={`w-full py-2 rounded-full ${cssStyle}`}>
-                {answer}
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+      <Answer answers={QUESTION[questionIndex].answers} userAsnwer={userAsnwer} asnwerState={asnwerState} onAnswer={handleUserAnswer} />
     </div>
   );
 }
