85341bb8b6d1a64a2bed9b8a02299f886bec349a
Fahmi Zain <fahmizain000@gmail.com>
2024-02-13 19:53:33 +0700

    feat: adding timer

---
+++ src/components/Timer.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+function Timer({ timeout, onTimeOut }) {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  useEffect(() => {
+    const timer = setTimeout(onTimeOut, timeout);
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [onTimeOut, timeout]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prev) => prev - 100);
+    }, 100);
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, []);
+
+  return (
+    <>
+      <progress value={remainingTime} max={timeout} />
+    </>
+  );
+}
+
+export default Timer;
