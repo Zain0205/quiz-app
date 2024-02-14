2da8173f1549ce9d9f88da936f2597273215155f
Fahmi Zain <fahmizain000@gmail.com>
2024-02-14 21:29:51 +0700

    update header text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <>
       <header className="text-center bg-sky-900 p-4 rounded-md">
-        <h1 className="text-2xl font-semibold text-white">Ini Quiz Tod</h1>
+        <h1 className="text-2xl font-semibold text-white">Ini Quiz</h1>
       </header>
     </>
   );
