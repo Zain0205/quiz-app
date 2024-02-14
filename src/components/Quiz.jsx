76c7b8cf2f8d8c30ac62b2f4b2837b06bf661a86
Fahmi Zain <fahmizain000@gmail.com>
2024-02-14 20:45:55 +0700

    cleaning Quiz components

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,33 +1,19 @@
-import { useState, useCallback, useRef } from "react";
+import { useState, useCallback } from "react";
 import QUESTION from "../question.js";
-import Timer from "./TImer.jsx";
-import Answer from "./Answer.jsx";
+import Question from "./Question.jsx";
 
 function Quiz() {
-    const [asnwerState, setAnswerState] = useState("");
   const [userAsnwer, setUserAnswer] = useState([]);
-  const questionIndex = asnwerState === "" ? userAsnwer.length : userAsnwer.length - 1;
+  const questionIndex = userAsnwer.length;
 
   const handleUserAnswer = useCallback(
     (ans) => {
-      setAnswerState("answered");
       setUserAnswer((prev) => {
         return [...prev, ans];
       });
-
-      setTimeout(() => {
-        if (QUESTION[questionIndex].answers[0] === ans) {
-          setAnswerState("correct");
-        } else {
-          setAnswerState("wrong");
-        }
-
-        setTimeout(() => {
-          setAnswerState("");
-        }, 2000);
-      }, 1000);
     },
-    [questionIndex]
+
+    []
   );
 
   const handleSkipAnswer = useCallback(() => handleUserAnswer(null), [handleUserAnswer]);
@@ -41,13 +27,9 @@ function Quiz() {
     );
   }
 
-  
-
   return (
     <div className="bg-sky-900 text-white w-full rounded-md px-4 flex flex-col gap-y-5 items-center py-5">
-      <Timer key={questionIndex} timeout={10000} onTimeOut={() => handleSkipAnswer(null)} />
-      <h1 className="text-2xl font-semibold">{QUESTION[questionIndex].question}</h1>
-      <Answer answers={QUESTION[questionIndex].answers} userAsnwer={userAsnwer} asnwerState={asnwerState} onAnswer={handleUserAnswer} />
+      <Question key={questionIndex} index={questionIndex} skipQuestion={handleSkipAnswer} selectQuestion={handleUserAnswer} />
     </div>
   );
 }
