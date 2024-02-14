ce274132b2f122368eb42c14d3dd7e9c9862f5e4
Fahmi Zain <fahmizain000@gmail.com>
2024-02-14 21:38:33 +0700

    updating Question components add new timer

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,44 +1,53 @@
 import { useState } from "react";
 import QUESTION from "../question";
 
-import Timer from './Timer'
-import Answer from './Answer'
+import Timer from "./Timer";
+import Answer from "./Answer";
 
-function Question({index, selectQuestion, skipQuestion}) {
-
+function Question({ index, selectQuestion, skipQuestion }) {
   const [answer, setAnswer] = useState({
-    selectedAnswer: '',
-    isCorrect: null
-  })
+    selectedAnswer: "",
+    isCorrect: null,
+  });
+
+  let timer = 10000;
+
+  if (answer.selectedAnswer) {
+    timer = 1000;
+  }
+
+  if(answer.isCorrect !== null){
+    timer = 2000
+  }
 
   const handleSelectAnswer = (ans) => {
     setAnswer({
       selectedAnswer: ans,
-      isCorrect: null
-    })
+      isCorrect: null,
+    });
 
     setTimeout(() => {
       setAnswer({
         selectedAnswer: ans,
-        isCorrect: QUESTION[index].answers[0] === ans 
-      })
+        isCorrect: QUESTION[index].answers[0] === ans,
+      });
 
       setTimeout(() => {
-        selectQuestion(ans)
-      }, 2000)
-    }, 1000)
-  }
-  
-  let asnwerState = ''
+        selectQuestion(ans);
+      }, 2000);
+    }, 1000);
+  };
 
-  if(answer.selectedAnswer && answer.isCorrect != null){
-    asnwerState = answer.isCorrect ? 'correct' : 'wrong'
-  } else if (answer.selectedAnswer){
-    asnwerState = "answered"
+  let asnwerState = "";
+
+  if (answer.selectedAnswer && answer.isCorrect != null) {
+    asnwerState = answer.isCorrect ? "correct" : "wrong";
+  } else if (answer.selectedAnswer) {
+    asnwerState = "answered";
   }
   return (
     <>
-      <Timer timeout={10000} onTimeOut={skipQuestion} />
+      <Timer key={timer} timeout={timer} onTimeOut={answer.selectedAnswer === '' ? skipQuestion : null} />
       <h1 className="text-2xl font-semibold">{QUESTION[index].question}</h1>
       <Answer answers={QUESTION[index].answers} userAsnwer={answer.selectedAnswer} asnwerState={asnwerState} onAnswer={handleSelectAnswer} />
     </>
