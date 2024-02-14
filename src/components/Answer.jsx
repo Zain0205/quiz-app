20b76c124871edd988cef6fc0c529abe5105f6d1
Fahmi Zain <fahmizain000@gmail.com>
2024-02-14 20:45:07 +0700

    Update Answer.jsx

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -8,12 +8,13 @@ function Answer({userAsnwer, answers, asnwerState, onAnswer}) {
     shuffeledAnswer.current.sort(() => Math.random() - 0.5);
   }
 
+
   return (
     <>
       <ul className="text-center flex flex-col gap-y-5 w-full">
         {shuffeledAnswer.current.map((answer, i) => {
           let cssStyle = "";
-          const isSelected = userAsnwer[userAsnwer.length - 1] === answer;
+          const isSelected = userAsnwer === answer;
 
           if (asnwerState === "answered" && isSelected) {
             cssStyle = "bg-amber-500";
@@ -28,7 +29,7 @@ function Answer({userAsnwer, answers, asnwerState, onAnswer}) {
           }
           return (
             <li className={`w-full bg-sky-700 rounded-full text-xl hover:bg-indigo-600 cursor-pointer`} key={i}>
-              <button onClick={() => onAnswer(answer)} className={`w-full py-2 rounded-full ${cssStyle}`}>
+              <button disabled={asnwerState !== ''} onClick={() => onAnswer(answer)} className={`w-full py-2 rounded-full ${cssStyle}`} >
                 {answer}
               </button>
             </li>
