8799c4fdb22ae9f94246673030bb753098da8f67
Fahmi Zain <fahmizain000@gmail.com>
2024-02-14 20:44:46 +0700

    update question

--- src/question.js
+++ src/question.js
@@ -2,42 +2,42 @@ export default [
   {
     id: "q1",
     question: "Pertanyaan 1",
-    answers: ["benar", "salah", "salah", "salah"],
+    answers: ["benar", "salah1", "salah2", "salah3"],
   },
   {
     id: "q2",
     question: "Pertanyaan 2",
-    answers: ["benar", "salah", "salah", "salah"],
+    answers: ["benar", "wrong", "false", "salah4"],
   },
   {
     id: "q3",
     question: "Pertanyaan 3",
-    answers: ["benar", "salah", "salah", "salah"],
+    answers: ["benar", "salahw", "salaha", "salahb"],
   },
   {
     id: "q4",
     question: "Pertanyaan 4",
-    answers: ["benar", "salah", "salah", "salah"],
+    answers: ["benar", "salahf", "salahd", "salahr"],
   },
   {
     id: "q5",
     question: "Pertanyaan 5",
-    answers: ["benar", "salah", "salah", "salah"],
+    answers: ["benar", "falsy", "undifined", "null"],
   },
   {
     id: "q6",
     question: "Pertanyaan 6",
-    answers: ["benar", "salah", "salah", "salah"],
+    answers: ["benar", "!benar", "salah banget", "salah woy"],
   },
   {
     id: "q7",
     question: "Pertanyaan 7",
-    answers: ["benar", "salah", "salah", "salah"],
+    answers: ["benar", "salah ini", "jangan pilih ini", "salah salah salah"],
   },
   {
     id: "q8",
     question: "Pertanyaan 8",
-    answers: ["benar", "salah", "salah", "salah"],
+    answers: ["benar", "salah wwwww", "salah 2111212", "salah 123"],
   },
   {
     id: "q9",
