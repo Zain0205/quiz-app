d348ef3246cd6a43a8bed3ea8cd9befb226a957e
Fahmi Zain <fahmizain000@gmail.com>
2024-02-16 22:45:38 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Quiz from "./components/Quiz";
 function App() {
   return (
     <>
-      <div className="bg-sky-500 h-screen px-4 flex flex-col justify-center gap-y-6">
+      <div className="bg-sky-500 min-h-screen py-20 px-4 flex flex-col justify-center gap-y-6">
         <Header />
         <Quiz />
       </div>
