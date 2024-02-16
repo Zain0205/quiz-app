7356d53d866c25b1e13347deffc1e5f7da26b73c
Fahmi Zain <fahmizain000@gmail.com>
2024-02-16 22:45:16 +0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <>
-      <header className="text-center bg-sky-900 p-4 rounded-md">
+      <header className="text-center w-full md:max-w-[60%] lg:max-w-[40%] mx-auto bg-sky-900 p-4 rounded-md">
         <h1 className="text-2xl font-semibold text-white">Ini Quiz</h1>
       </header>
     </>
